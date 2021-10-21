9c5cb3be6ec5744094113eeab0461d103aab90ea
stell <cristinatoes@gmail.com>
2021-10-21 13:27:49 -0500

    Initial commit

---
+++ src/main/java/finitefields.java
@@ -0,0 +1,19 @@
+public class finitefields{
+    public void multi(){
+
+    }
+
+    public void table(){
+
+    }
+
+    public void searchInv(){
+
+    }
+
+    public static void main (String[]args){
+        //mx
+        //gx
+        //fx
+    }
+}
